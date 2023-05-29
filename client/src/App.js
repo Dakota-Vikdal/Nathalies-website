7d8cfb52918757caeebde58ddc17bccb496638be
Dakota Vikdal <dakota.james19@hotmail.com>
2023-05-29 17:39:13 -0400

    react beginnings

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './stylesheets/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This is going to be Nathalie's website believe it or not</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
